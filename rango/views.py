e51b59a83ce0d5efe3cdd3eb3841070f1643c27d
rdmaloney <2026124M@student.gla.ac.uk>
2019-02-09 22:27:08 +0000

    Updated to Chapter 3

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+   # First construct a dictionary to pass to the template - this is it's "context".
+    # The Key value should match the name of the {{ ToBeReplaced }} -> "ToBeReplaced"
+    # text in the template.
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #               ^Key to replace in template   ^Value
+
+    return render(request, 'rango/index.html', context=context_dict)
+    # Django finds the template file as refrenced in the settings, and takes the context to adapt
+    # the template to add content "context". When Django encounters {{ key }} in the template file
+    # it looks up the name in the context dictionary to find out what to replace it with.
+
 
 def about(request):
-    return HttpResponse("Rango says here is the about page")
+    return render(request, 'rango/about.html')
