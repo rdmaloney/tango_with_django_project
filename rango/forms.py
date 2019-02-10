72c3d9b5d85d3003b16686a0407699260ce7d7f9
rdmaloney <2026124M@student.gla.ac.uk>
2019-02-10 00:49:47 +0000

    Updated to chapter 7

---
+++ rango/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=Category.maxCharFeildLength, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # an inner class to provide extra info on the form.
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Category
+        fields = ('name',)
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=Page.maxCharFeildLength, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
+
+    class Meta:
+        model = Page
+
+        # By excluding the category field it avoids problems with user adding categories(foreign key) here that do not exist.
+        exclude = ('category',)
+        # This is a little simpler than listing just the feilds you want to show. 
